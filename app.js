324d0e131c51589f2e8f9fbfb0e6597052591b2e
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-01-16 15:32:32 -0400

    Create game(), and add a couple of functions

--- app.js
+++ app.js
@@ -1,11 +1,58 @@
-const player = function () {
-    return {};
-}
 
-const gameBoard = (function() {
-    return {}
-})();
+const game = (function() {
 
-const game = (function () {
-    return {};
-})()
+  const obj = {
+    '1:1': '',
+    '1:2': '',
+    '1:3': '',
+    '2:1': '',
+    '2:2': '',
+    '2:3': '',
+    '3:1': '',
+    '3:2': '',
+    '3:3': '',
+  }
+  
+  function render() {
+    return `
+        ${obj['1:1']} | ${obj['1:2']} | ${obj['1:3']}
+      -----------
+        ${obj['2:1']} | ${obj['2:2']} | ${obj['2:3']}
+      -----------
+        ${obj['3:1']} | ${obj['3:2']} | ${obj['3:3']}
+    `
+  }
+
+  
+
+
+  function gameController () {
+
+  }
+
+
+
+  const players = (function(name1, name2) {
+    return {
+      player1: {
+        name: name1,
+        mark: 'X'
+      },
+      player2: {
+        name: name2,
+        mark: 'O'
+      }
+    }
+  })();
+
+
+
+  return {}
+})
+
+game()
+
+
+
+
+
