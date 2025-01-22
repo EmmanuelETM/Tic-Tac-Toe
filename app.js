450304b1ee3dc3d9e9ca112b0472dab102cabc63
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-01-21 22:53:46 -0400

    Working on some stuff on the console

--- app.js
+++ app.js
@@ -1,37 +1,71 @@
-const game = (function() {
-  const obj = {
-    '1:1': '',
-    '1:2': '',
-    '1:3': '',
-    '2:1': '',
-    '2:2': '',
-    '2:3': '',
-    '3:1': '',
-    '3:2': '',
-    '3:3': '',
-  }
+const Cell = function() {
+  let value = '';
   
-  function render() {
-
+  const getValue = () => value;
+  const addMark = (mark) => {
+    value = mark;
   }
 
-  const players = (function(name1, name2) {
-    return {
-      player1: {
-        name: name1,
-        mark: 'X'
-      },
-      player2: {
-        name: name2,
-        mark: 'O'
-      },
-    }
-  })();
+  return {
+    getValue, 
+    addMark
+  }
+}
 
-  return {}
+const gameBoard = (function() {
+  const board = new Array(9).fill('');
+  
+  const getBoard = () => board;
+
+  const fillSpot = (index, mark) => {
+    if(board[index] === '' && index < board.length && index >= 0){
+      board[index] = mark;
+    }
+    else {
+      console.log('invalid move');
+    }
+  }
+
+  fillSpot(2, 'X'); 
+  return {getBoard};
+})(); 
+
+console.log(gameBoard.getBoard())
+const players = (function() {
+  return {
+    player1: {
+      name: 'elpepe',
+      mark: 'X'
+    },
+    player2: {
+      name: 'mariaairam',
+      mark: 'O'
+    },
+  }
+})();
+
+
+const GameController = (function() {
+
+  let currentPlayer = players.player1;
+
+  const getCurrentPlayer = () => currentPlayer;
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === players.player1 ? players.player2 : players.player1;
+  }
+
+  const playRound = () => {
+    
+  }
+
+
+})();
+
+const game = (function() {
+
 })
 
-game();
+
 
 
 
