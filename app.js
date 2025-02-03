948b5f21ee8bf7d7c929edb90a558b8cf2b22d2b
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-02-02 20:42:53 -0400

    Finished game on the console, adding ui now

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ function Cell () {
 const GameBoard = (() => {
     const board = new Array(9).fill(null).map(() => Cell());
 
-    const getBoard = () => board;
+    const getRawBoard = () => board;
+
+    const getBoard = () => board.map(cell => cell.getMark());
 
     const placeMark = (position, mark) => {
         const cell = board[position];
@@ -25,14 +27,15 @@ const GameBoard = (() => {
 
     const resetBoard = () => board.forEach(cell => cell.setMark(""));
 
-    return { getBoard, placeMark, resetBoard };
+    return { getBoard, getRawBoard, placeMark, resetBoard };
 })();
-
 
 const GameController = (()=> {
     let count = 0;
+
+    //players stuff
     const players = [
-        {
+        {                                                        
             name: "john",
             mark: "X",
         },
@@ -41,9 +44,10 @@ const GameController = (()=> {
             mark: "O"
         }
     ]
-
     let activePlayer = players[0];
+    const switchPlayer = () => activePlayer = activePlayer === players[0] ? players[1] : players[0];
 
+    //Check for wins
     let winCombos = [
         [0, 1, 2], // Top Row
         [3, 4, 5], // Middle Row
@@ -54,51 +58,52 @@ const GameController = (()=> {
         [0, 4, 8], // Left Diagonal
         [2, 4, 6], // Right Diagonal
     ];
-    
-
-    const getActivePlayer = () => activePlayer;
-    const switchPlayer = () => activePlayer = activePlayer === players[0] ? players[1] : players[0];
 
     const checkWinner = (board, winCombos) => {
         for (const combo of winCombos) {
             const [a, b, c] = combo;
-            if (board[a].getMark() && 
-                board[a].getMark() === board[b].getMark() &&
-                board[a].getMark() === board[c].getMark()) {
+            if (board[a] && 
+                board[a] === board[b] &&
+                board[a] === board[c]) {
                 return true;
             }
         }
         return false;
     };
+
+    const playGame = () => {
+        while (true) {
+            count++;
+            const board = GameBoard.getBoard();
+            const choice = parseInt(prompt("pick position: "));
+            const move = GameBoard.placeMark(choice, activePlayer.mark);
     
-    for(let i = 0; i < 8; ++i){
-        const board = GameBoard.getBoard();
-        const choice = prompt("pick position: ");
-        const move = GameBoard.placeMark(choice, activePlayer.mark);
-        console.log(board);
-
-        if (!move) {
-            console.log("invalid move");
-            i--;
-            continue;
+            if (!move) {
+                console.log("invalid move");
+                count--;
+                continue;
+            }
+    
+            if(checkWinner(board, winCombos)) {
+                console.log(`${activePlayer.name} wins`);
+                break;
+            }
+    
+            if(board.every(mark => mark !== "") || count >= 9) {
+                console.log("tie");
+                break;
+            }
+            
+            switchPlayer();
         }
-
-        console.log(board[choice].getMark());
-        
-        const check = checkWinner(board, winCombos);
-
-        if(check) {
-            console.log(`${activePlayer.name} wins`);
-            break;
-        }
-
-        switchPlayer();
     }
+    
+    return { playGame, players }
 })();
 
 
 const DisplayController = (()=> {
-
+    
 })();
 
 
